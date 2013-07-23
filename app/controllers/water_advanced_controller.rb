04af9b3b5b6c159ea12ee2dc3cc571bd1b1ad087
George Miguel <gmigueluk@gmail.com>
2013-07-23 01:49:11 +0100

    advanced i18n (more)

--- app/controllers/water_advanced_controller.rb
+++ app/controllers/water_advanced_controller.rb
@@ -2,10 +2,17 @@ class WaterAdvancedController < ApplicationController
 
 
   layout "water_advanced_layout"
+
   def index
 
 
 
+  end
+
+  def report
+
+
+    render layout: "report_water_advanced"
   end
 
 end
