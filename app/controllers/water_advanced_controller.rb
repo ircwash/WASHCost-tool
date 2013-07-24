c1c296a80f2a01882608290ce4f8256ed9165274
George Miguel <gmigueluk@gmail.com>
2013-07-24 22:10:58 +0100

    more advanced_water sessioning

--- app/controllers/water_advanced_controller.rb
+++ app/controllers/water_advanced_controller.rb
@@ -9,6 +9,9 @@ class WaterAdvancedController < ApplicationController
     else
       init_session_form
     end
+
+    puts "WATER ADVANCED"
+    puts flash
   end
 
 
@@ -18,34 +21,37 @@ class WaterAdvancedController < ApplicationController
       :calculator_type => get_calculator_type,
 
       :country => get_country(params[:country]),
-      :area_type => get_indexed_value('form.water_advanced.area_type.a', :area_type),
+      :region => unchecked(:region, params[:region]),
+      :city=> unchecked(:city, params[:city]),
+      :density => get_indexed_value('form.water_advanced.density.answers.a', :density),
+      :area_type => get_indexed_value('form.water_advanced.area_type.answers.a', :area_type),
 
       :who_finances =>  get_indexed_value('form.water_advanced.management.shared.answers.a', :who_finances),
       :who_owns =>      get_indexed_value('form.water_advanced.management.shared.answers.a', :who_owns),
       :who_safeguards =>get_indexed_value('form.water_advanced.management.shared.answers.a', :who_safeguards),
       :who_enforces =>  get_indexed_value('form.water_advanced.management.shared.answers.a', :who_enforces),
       :who_repairs =>   get_indexed_value('form.water_advanced.management.shared.answers.a', :who_repairs),
-      :annual_income => get_annual_income(params[:annual_income]),
+      :annual_income => unchecked(:annual_income, nil),
 
       :supply_system => get_indexed_value('form.water_advanced.supply_system.answers.a', :supply_system),
-      :inauguration =>  get_inauguration(params[:inauguration]),
+      :inauguration =>  unchecked(:inauguration, nil),
       :water_sources => get_indexed_value('form.water_advanced.water_sources.answers.a', :water_sources),
       :water_storage => get_indexed_value('form.water_advanced.water_storage.answers.a', :water_storage),
       :treatment =>     get_indexed_value('form.water_advanced.treatment.answers.a', :treatment),
       :power_supply =>  get_indexed_value('form.water_advanced.power_supply.answers.a',:power_supply),
-      :transmission =>  get_transmission(params[:transmission]),
-      :piped =>         get_piped(params[:piped]),
+      :transmission =>  unchecked(:transmission, nil),
+      :piped =>         unchecked(:piped, nil),
 
-      :total_cost =>    get_total_cost(params[:total_cost]),
+      :total_cost =>    get_total_cost(params[:hardware],params[:software]),
 
-      :hardware =>      get_hardware(params[:hardware]),
-      :software =>      get_software(params[:software]),
-      :maintenance =>   get_maintenance(params[:maintenance]),
-      :direct =>        get_direct(params[:direct]),
-      :indirect =>      get_indirect(params[:indirect]),
+      :hardware =>      unchecked(:hardware, nil),
+      :software =>      unchecked(:software, nil),
+      :maintenance =>   unchecked(:maintenance, nil),
+      :direct =>        unchecked(:direct, nil),
+      :indirect =>      unchecked(:indirect, nil),
 
-      :loan =>          get_loan(params[:loan]),
-      :payback =>       get_payback(params[:payback])
+      :loan =>          unchecked(:loan, nil),
+      :payback =>       unchecked(:payback, nil)
     }
 
     flash[:results]= results
@@ -61,8 +67,8 @@ class WaterAdvancedController < ApplicationController
   def copy_session_form_values_to_flash
 
     form_params= [
-        :country, :area_type,
-        :who_finances,:who_owns, :who_safeguards, :who_enforces, :who_repair, :annual_income,
+        :country, :region, :city, :area_type, :density,
+        :how_managed, :who_finances, :who_owns, :who_safeguards, :who_enforces, :who_repairs, :annual_income,
         :supply_system, :inauguration, :water_sources, :water_sources, :water_storage, :treatment, :power_supply, :transmission, :piped,
         :total_cost, :hardware, :software, :maintenance, :direct, :indirect,
         :loan, :payback
@@ -71,6 +77,8 @@ class WaterAdvancedController < ApplicationController
     form_params.each do |param|
       flash[param]= session[:water_advanced][param]
     end
+
+    puts flash
   end
 
 
@@ -88,15 +96,26 @@ class WaterAdvancedController < ApplicationController
       country= country.name
     end
 
-    add_to_session_form('country', country)
+    add_to_session_advanced('country', country)
     return country
   end
 
-  def get_annual_income(annual_income)
+  def unchecked(key, value)
 
-    return annual_income
+    add_to_session_advanced(key, value)
+    return value
 
   end
+
+  def get_total_cost(hardware, software)
+
+    total_cost= 'Please enter both Hardware & Software values'
+    if hardware && software
+      return hardware+software
+    end
+
+  end
+
 
   def get_indexed_value(i18nPrefix, key)
 
@@ -107,77 +126,18 @@ class WaterAdvancedController < ApplicationController
     if index
       text= I18n.t i18nPrefix+index
 
-      add_to_session_form(key, index)
+      add_to_session_advanced(key, index)
     end
 
     return text
   end
 
-  def get_inauguration(inauguration)
-    #YeAR TEST
-    return inauguration
-  end
 
-
-  def get_transmission(transmission)
-     # INT TEST
-    return transmission
-  end
-
-  def get_piped(piped)
-
-    # % TEST
-    return piped
-  end
-
-  def get_total_cost(total_cost)
-
-
-    return 'blergh'
-  end
-
-  def get_hardware(hardware)
-
-    return 'blergh'
-  end
-
-  def get_software(software)
-
-    return 'blergh'
-  end
-
-  def get_maintenance(maintenance)
-
-    return 'blergh'
-  end
-
-  def get_direct(direct)
-
-    return 'blergh'
-  end
-
-  def get_indirect(indirect)
-
-    return 'blergh'
-  end
-
-  def get_loan(loan)
-
-    return 'blergh'
-  end
-
-  def get_payback(payback)
-
-    return 'blergh'
-  end
-
-
-
-  def add_to_session_form(key, value)
+  def add_to_session_advanced(key, value)
     form= session[:water_advanced].present? ? session[:water_advanced] : Hash.new(0)
 
     if !form[key].present?
-      increase_complete_percent(:water_advanced)
+      increase_complete_percent(:water_advanced_completed)
     end
 
     form[key]= value
