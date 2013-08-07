6fd32477c6098271ee92379d34d387202ae876d4
Johaned <johan.tique@codescrum.com>
2013-08-07 11:37:47 -0500

    #0 fix bug in water and sanitation navigation top bar

--- app/helpers/water_basic_helper.rb
+++ app/helpers/water_basic_helper.rb
@@ -79,7 +79,7 @@ module WaterBasicHelper
               }
           },
           :service => {
-              :offset => '-280',
+              :offset => '-730',
               :items => {
                   'providing' => (I18n.t 'nav.main.service.items.providing'),
                   'impermeability' => (I18n.t 'nav.main.service.items.impermeability'),
@@ -108,7 +108,7 @@ module WaterBasicHelper
             }
         },
         :service => {
-            :offset => '-250',
+            :offset => '-350',
             :items => {
                 'time' => (I18n.t 'nav.main.service.items.time'),
                 'quantity' => (I18n.t 'nav.main.service.items.quantity'),
