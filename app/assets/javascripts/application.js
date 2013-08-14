daf063287e2db9db04c844057d2cc32a3b1208e7
Johaned <johan.tique@codescrum.com>
2013-08-14 16:23:40 -0500

    add logic to resize the header title on each basic tool pages when this title exceeds 94 px

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,10 +31,8 @@ $(document).ready(function()
     function radioInputTick() {
         // target to change the info box regarding to item selected
         target_select = $('input[name="target-select"]').data("target");
-
         var $tick = $('.tickCheckBox'),
             $inputTick = $('.tickCheckBox input');
-
         $('.tickCheckBox').on('click', function(e)
         {
             e.preventDefault();
@@ -45,37 +43,41 @@ $(document).ready(function()
                 data : "trigger="+target_select+"&index="+$(this).find('input').val(),
                 type : "POST",
             });
-
             if($inputTick.length)
             {
                 $inputTick.parent().removeClass('ticked');
-
                 if ($(this).hasClass('ticked')) {
                     $(this).removeClass('ticked');
-
                 } else {
                     $(this).addClass('ticked');
-
                     var targetHiddenInput= $inputTick.attr('name').split('_')[1];
                     var clickedLabelsInputValue= $(this).find('input').val();
-
                     $('input[name="'+targetHiddenInput+'"]').val(clickedLabelsInputValue);
                 }
             }
         });
 
     } radioInputTick();
-
     function readMore() {
-
         var $target = $('.readMore');
-
         $target.on('click', function(e) {
-
             $(this).next().slideToggle();
             e.preventDefault();
-
         });
-        
     } readMore();
+
+    //Function to resize the size of header in each page of basic tools
+    header_basic_tool = $('.basic_tool_header_item');
+    heihgt_header = header_basic_tool.height();
+    HEIGHT_HEADER_LIMIT = 94;
+    HEADER_FONT_SIZE_BASE = 40
+    if(header_basic_tool.length>0 && heihgt_header> HEIGHT_HEADER_LIMIT){
+        font_size = HEADER_FONT_SIZE_BASE;
+        while(heihgt_header > HEIGHT_HEADER_LIMIT){
+            font_size--;
+            header_basic_tool.css('font-size',font_size+'px');
+            heihgt_header = header_basic_tool.height();
+        }
+    }
+
 });
