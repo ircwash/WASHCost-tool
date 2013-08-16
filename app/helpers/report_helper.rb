61d9906c7c90f09704f44e381e32c241a40b0eee
Jairo Diaz <jairo.diaz@codescrum.com>
2013-08-16 17:32:14 +0100

    Removed unused function compute_rating_from_score

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,16 +1,4 @@
 module ReportHelper
-  def compute_rating_from_score(score)
-    if score >= 7.5
-      3
-    elsif score >= 5 && score < 7.5
-      2
-    elsif score >= 2 && score < 5
-      1
-    else
-      0
-    end
-  end
-
   def rating_for_expenditure(val, min, max)
     if val < min
       0.5
--- spec/helpers/report_helper_spec.rb
+++ spec/helpers/report_helper_spec.rb
@@ -1,20 +1,7 @@
 require_relative  '../spec_helper'
 
 describe ReportHelper, :type => :helper do
-
   subject { Class.new.include ReportHelper }
-
-  describe "#compute_rating_from_score" do
-    pairs = [ [0, 0], [1, 0], [2, 1], [3, 1], [4, 1], [5, 2], [6, 2], [7, 2],
-             [7.5, 3], [8, 3], [9, 3], [10, 3] ]
-    pairs.each do |pair|
-      context "when score is #{pair[0]}" do
-        it "should be #{pair[1]}" do
-          expect(compute_rating_from_score(pair[0])).to eq(pair[1])
-        end
-      end
-    end
-  end
 
   describe "#rating_for_expenditure" do
     min, max = 3, 7
