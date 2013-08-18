7c96730f03e01e123b7f83585afee0db64215ae0
Jairo Diaz <jairo.diaz@codescrum.com>
2013-08-18 11:45:54 +0100

    Made sliders update information for sliding even for real-time updates

--- app/assets/javascripts/sanitation_basic.js
+++ app/assets/javascripts/sanitation_basic.js
@@ -3,28 +3,6 @@ $(document).ready(function(){
     // Option buttons that are checked should be highlighted.
     $( "[checked='checked']" ).parent().addClass('ticked');
 
-    // Page 2
-    if( document.getElementById('householdSlider')){
-
-        //default
-        $('input[name="household"]').val(0);
-
-        $( "#householdSlider" ).slider({
-            step: 1,
-            min: 1,
-            max: 20,
-            change: function( event, ui ) {
-                $('input[name="household"]').val(ui.value);
-                $('#householdValue').html(ui.value);
-            }
-        });
-
-        var slider_value = document.getElementById('householdSlider').getAttribute("data-slider_value");
-        $( "#householdSlider" ).slider({value: slider_value});
-    }
-
-    // Page 3
-
     // Page 4
     if( document.getElementById('populationSlider')){
 
@@ -32,17 +10,20 @@ $(document).ready(function(){
         //default
         $('input[name="capital"]').val(0);
 
+        slider_display = function (event, ui) {
+            var value = Math.round(Number(LOG_SLIDER.expon(ui.value)/100)) * 100;
+            $('input[name="population"]').val(value);
+            var stringVal= '';
+            stringValue= value.toLocaleString();
+            $('#populationValue').html(stringValue);
+        };
+
         $( "#populationSlider" ).slider({
             min: LOG_SLIDER.gMinPrice,
             max: LOG_SLIDER.gMaxPrice,
             step: 500,
-            change: function( event, ui ) {
-                var value = Math.round(Number(LOG_SLIDER.expon(ui.value)/100)) * 100;
-                $('input[name="population"]').val(value);
-                var stringVal= '';
-                stringValue= value.toLocaleString();
-                $('#populationValue').html(stringValue);
-            }
+            change: slider_display,
+            slide: slider_display
         });
 
         var slider_value = Number(document.getElementById('populationSlider').getAttribute("data-slider_value"));
@@ -72,13 +53,15 @@ $(document).ready(function(){
         label_last.css('margin-left',css_above_value-(bar_slider_lenght/2)-offset_cursor+"px");
 
         capitalSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
+        slider_display = function( event, ui ) {
+            inputCapital.val(ui.value);
+            $('#capitalValue').html(ui.value);
+        }
         capitalSlider.slider({
             min: inputCapital.data('min-value'),
             max: inputCapital.data('max-value'),
-            change: function( event, ui ) {
-                inputCapital.val(ui.value);
-                $('#capitalValue').html(ui.value);
-            }
+            change: slider_display,
+            slide: slider_display
         });
 
         var slider_value = document.getElementById('capitalSlider').getAttribute("data-slider_value");
@@ -108,13 +91,15 @@ $(document).ready(function(){
         label_last.css('margin-left',css_above_value-(bar_slider_lenght/2)-offset_cursor+"px");
 
         recurrentSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
+        slider_display = function( event, ui ) {
+            inputRecurrent.val(ui.value);
+            $('#recurrentValue').html(ui.value);
+        }
         recurrentSlider.slider({
             min: inputRecurrent.data('min-value'),
             max: inputRecurrent.data('max-value'),
-            change: function( event, ui ) {
-                inputRecurrent.val(ui.value);
-                $('#recurrentValue').html(ui.value);
-            }
+            change: slider_display,
+            slide: slider_display
         });
 
         var slider_value = document.getElementById('recurrentSlider').getAttribute("data-slider_value");
