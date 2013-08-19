4b2be03f884087ccc87ea61509b9c7420a6ed435
Jairo Diaz <jairo.diaz@codescrum.com>
2013-08-19 15:21:44 +0100

    Smooth slider for water basic calculator

--- app/assets/javascripts/water_basic.js
+++ app/assets/javascripts/water_basic.js
@@ -14,17 +14,19 @@ $(document).ready(function(){
 
         //default
         $('input[name="population"]').val(0);
+        slider_display = function( event, ui ) {
+            var value = Math.round(Number(LOG_SLIDER.expon(ui.value)/100)) * 100;
+            $('input[name="population"]').val(value);
+            var stringVal = '';
+            stringValue= value.toLocaleString();
+            $('#populationValue').html(stringValue);
+        };
 
         $( "#populationSlider" ).slider({
             min: LOG_SLIDER.gMinPrice,
             max: LOG_SLIDER.gMaxPrice,
-            change: function( event, ui ) {
-                var value = Math.round(Number(LOG_SLIDER.expon(ui.value)/100)) * 100;
-                $('input[name="population"]').val(value);
-                var stringVal = '';
-                stringValue= value.toLocaleString();
-                $('#populationValue').html(stringValue);
-            },
+            change: slider_display,
+            slide: slider_display
         });
 
         var slider_value = Number(document.getElementById('populationSlider').getAttribute("data-slider_value"));
@@ -54,13 +56,15 @@ $(document).ready(function(){
         label_last.css('margin-left',css_above_value-(bar_slider_lenght/2)-offset_cursor+"px");
 
         capitalSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
+        slider_display = function(event,ui) {
+            inputCapital.val(ui.value);
+            $('#capitalValue').html(ui.value);
+        };
         capitalSlider.slider({
             min: inputCapital.data('min-value'),
             max: inputCapital.data('max-value'),
-            change: function( event, ui ) {
-                inputCapital.val(ui.value);
-                $('#capitalValue').html(ui.value);
-            }
+            change: slider_display,
+            slide: slider_display
         });
 
         var slider_value = document.getElementById('capitalSlider').getAttribute("data-slider_value");
@@ -89,14 +93,16 @@ $(document).ready(function(){
         label_last = $('.labelScale li.last span');
         label_last.css('margin-left',css_above_value-(bar_slider_lenght/2)-offset_cursor+"px");
 
-       recurrentSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
+        recurrentSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
+        slider_display = function( event, ui ) {
+            inputRecurrent.val(ui.value);
+            $('#recurrentValue').html(ui.value);
+        };
         recurrentSlider.slider({
             min: inputRecurrent.data('min-value'),
             max: inputRecurrent.data('max-value'),
-            change: function( event, ui ) {
-                inputRecurrent.val(ui.value);
-                $('#recurrentValue').html(ui.value);
-            }
+            change: slider_display,
+            slide: slider_display
         });
 
         var slider_value = document.getElementById('recurrentSlider').getAttribute("data-slider_value");
@@ -109,34 +115,18 @@ $(document).ready(function(){
         //default
         $('input[name="time"]').val(0);
         timeSlider = $("#timeSlider");
+        slider_display = function(event, ui) {
+            $('input[name="time"]').val(ui.value);
+            $('#timeValue').html(I18n["en"]["form"]["water_basic"]["time"]['answers']['a'+ui.value]);
+        }
         timeSlider.slider({
             min: 0,
             max: 3,
             step: 1,
-            change: function( event, ui ) {
-                $('input[name="time"]').val(ui.value);
-                $('#timeValue').html(I18n["en"]["form"]["water_basic"]["time"]['answers']['a'+ui.value]);
-            }
+            change: slider_display,
+            slide: slider_display
         });
         slider_value = document.getElementById('timeValue').getAttribute("data-slider_value");
         timeSlider.slider({value: slider_value});
     }
-
-    // Page 7
-    if( document.getElementById('quantitySlider')){
-
-        //default
-        $('input[name="quantity"]').val(0);
-
-        $( "#quantitySlider" ).slider({
-            min: 0,
-            max: 3,
-            step: 1,
-            change: function( event, ui ) {
-                $('input[name="quantity"]').val(ui.value);
-                $('#quantityValue').html(db.quantity[ui.value].label);
-            }
-        });
-    }
-
 });
