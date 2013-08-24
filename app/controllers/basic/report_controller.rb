31c2ed4c9b170662614f604e612b0254508acdf2
Johan Tique <johan.tique@codescrum.com>
2013-08-23 19:29:45 -0500

    generate a controller to basic report actions

--- app/controllers/basic/report_controller.rb
+++ app/controllers/basic/report_controller.rb
@@ -1,3 +1,11 @@
-class ReportController
-  # To change this template use File | Settings | File Templates.
+class Basic::ReportController < ApplicationController
+  def save_report
+    tool_name = params[:tool_name]
+    title = params[:title]
+    form = session["#{params[:tool_name]}_basic_form".to_sym] || Hash.new(0)
+    current_user.basic_questionnaires.build(title: title, tool_name: tool_name, form: form)
+    respond_to do |format|
+      format.js
+    end
+  end
 end
