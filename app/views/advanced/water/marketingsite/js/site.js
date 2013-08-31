8d0e4a625b612b9b12cf8791840d43ebda9a7641
Cat Le-Huy <nacht@widerstand.org>
2013-08-31 17:45:49 +0100

    Revert "Merge pull request #5 from darrenangle/master"
    This reverts commit a561dad4b38ba8b46f02e608ddeb68069ebf48a0, reversing
    changes made to ba8c04e4d20c8a01abb4b2ad6a6764914db9b95e.

--- app/views/advanced/water/marketingsite/js/site.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-
-	$('#placeholder-image').click(function(){
-    	var video = '<iframe src="https://player.vimeo.com/video/73270180?autoplay=true" width="800" height="451" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-   		$(this).replaceWith(video);
-	});
-	
-});
