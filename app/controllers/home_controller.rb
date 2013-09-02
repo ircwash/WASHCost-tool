0726f4bba01e4e3a59567477d3410cefa8daed69
Johan Tique <johan.tique@codescrum.com>
2013-09-02 18:36:38 -0500

    fix bugs in dashboard and journey

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,8 +13,14 @@ class HomeController < ApplicationController
     @selection = Selection::ToolSelection.new(params[:selection])
     case @selection.index
       when 0
+        temp = session[:sanitation_basic_form]
+        reset_session
+        session[:sanitation_basic_form] = temp
         redirect_to cal_water_basic_path
       when 2
+        temp = session[:water_basic_form]
+        reset_session
+        session[:water_basic_form] = temp
         redirect_to cal_sanitation_basic_path
       else
         redirect_to root_path, flash: { popup_class_name: '.notification.subscriber' }
