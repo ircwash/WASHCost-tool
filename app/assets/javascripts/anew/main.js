9b3c9d62fd4310fffc5fb5745f7c4b342a8d01ed
Johan Tique <johan.tique@codescrum.com>
2013-09-09 18:53:21 -0500

    add namespace to radioButtonInputTick in main.js

--- app/assets/javascripts/anew/main.js
+++ app/assets/javascripts/anew/main.js
@@ -3,29 +3,32 @@ $(document).ready(function(){
 
     $('.chzn-select').chosen()
 
-    function radioInputTick() {
-        $('.tickCheckBox').on('click', function(e)
-        {
-            e.preventDefault();
-            console.log($(this));
-            inputTick = $(".tickCheckBox input[name='"+$(this).attr('for')+"']");
-            console.log(inputTick);
-            if(inputTick.length)
+    if($('.landing-container').length > 0){
+        function radioInputTick() {
+            $('.tickCheckBox').on('click', function(e)
             {
-                inputTick.parent().removeClass('ticked');
-                if ($(this).hasClass('ticked')) {
-                    $(this).removeClass('ticked');
-                } else {
-                    $(this).addClass('ticked');
-                    $('#'+$(this).data('target')).val($(this).find('input').val())
+                e.preventDefault();
+                console.log($(this));
+                inputTick = $(".tickCheckBox input[name='"+$(this).attr('for')+"']");
+                console.log(inputTick);
+                if(inputTick.length)
+                {
+                    inputTick.parent().removeClass('ticked');
+                    if ($(this).hasClass('ticked')) {
+                        $(this).removeClass('ticked');
+                    } else {
+                        $(this).addClass('ticked');
+                        $('#'+$(this).data('target')).val($(this).find('input').val())
+                    }
                 }
-            }
-        });
+            });
 
-    } radioInputTick();
-    // Option buttons that are checked should be highlighted.
-    option_cheched = $( "[checked='checked']" )
-    option_cheched.parent().addClass('ticked');
+        } radioInputTick();
+        // Option buttons that are checked should be highlighted.
+        option_cheched = $( "[checked='checked']" );
+        option_cheched.parent().addClass('ticked');
+    }
+
 
     // ---------> Resize the window according to spatial use
     if ($(window).height()>$("body").height()){
