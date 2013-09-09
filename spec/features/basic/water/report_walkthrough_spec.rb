f3037c8ccfdd6992851f800a2f1b64b88a206222
Johan Tique <johan.tique@codescrum.com>
2013-09-09 11:16:16 -0500

    add js to basic water specs and correct issue with simulated slider spec (miguel commit)

--- spec/features/basic/water/report_walkthrough_spec.rb
+++ spec/features/basic/water/report_walkthrough_spec.rb
@@ -25,10 +25,29 @@ describe "Basic Tool Walkthrough" do
       basic_water[:water].should == 1
     end
 
+    it 'selects the population', :js  do
+      visit '/water_basic/population'
+
+      slider_handle = page.find('.ui-slider-handle')
+      info_tab = page.find('.tab')
+      slider_handle.drag_to(info_tab)
+
+      click_button t('buttons.next')
+      current_path.should == '/water_basic/capital'
+      basic_water[:population].should == 1000000
+    end
+
     # a more semantic approach to naming the session
     # could be named "report" too
     def basic_water
       basic_water_session
+    end
+
+    # click one of the typical boxes in the UI when selecting icons
+    # @param label_name [String] the actual "class" for the label icon image
+    # e.g. boreHold_03, waterTool << css class names
+    def pick_box(label_name)
+      find("label.#{label_name}").click
     end
 
   end
--- spec/support/washcost_steps_helper.rb
+++ spec/support/washcost_steps_helper.rb
@@ -3,9 +3,11 @@ module WashCostStepsHelper
   # @param tool_name [String] the name of the tool can be either :water or :sanitation
   # @param tool_type [String] the type of the tool can be either :basic or :advanced
   def start_calculator(tool_name, tool_type)
-    visit cal_path
-    choose "#{tool_name}_tool"
-    choose "#{tool_type}_tool"
+    visit root_path
+
+    find("label.tool#{tool_name.to_s.capitalize}").click
+    find("label.tool#{tool_type.to_s.capitalize}").click
+
     click_button t('buttons.start')
   end
 end
