075c6a8f07613d4883d596049e04e2b794b297af
Johaned <johan.tique@codescrum.com>
2013-09-08 22:33:57 -0500

    add capital slider js logic

--- app/assets/javascripts/anew/slider-config.js
+++ app/assets/javascripts/anew/slider-config.js
@@ -24,4 +24,39 @@ $(document).ready(function(){
         populationSlider.slider({value: LOG_SLIDER.logposition(slider_value)});
     }
 
+    // Capital slider
+    capitalSlider = $( "#capitalSlider" );
+    if(capitalSlider.length > 0){
+        inputCapital = $('input[name="capital"]');
+        //default
+        inputCapital.val(0);
+        offset_cursor = 10;
+        bar_slider_lenght = 850.0;
+        slider_lenght = inputCapital.data('max-value') - inputCapital.data('min-value');
+        above_value = inputCapital.data('above-value');
+        below_value = inputCapital.data('below-value');
+        step_slider = bar_slider_lenght/slider_lenght;
+        css_above_value = above_value * step_slider + offset_cursor;
+        css_below_value = below_value * step_slider + offset_cursor;
+        //labels
+        label_first = $('.labelScale li.first span');
+        label_first.css('margin-left',css_below_value-offset_cursor+"px");
+        label_last = $('.labelScale li.last span');
+        label_last.css('margin-left',css_above_value-(bar_slider_lenght/2)-offset_cursor+"px");
+
+        capitalSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
+        slider_display = function(event,ui) {
+            inputCapital.val(ui.value);
+            $('#capitalValue').html(ui.value);
+        };
+        capitalSlider.slider({
+            min: inputCapital.data('min-value'),
+            max: inputCapital.data('max-value'),
+            change: slider_display,
+            slide: slider_display
+        });
+        slider_value = capitalSlider.attr("data-slider_value");
+        capitalSlider.slider({value: slider_value});
+    }
+
 });
