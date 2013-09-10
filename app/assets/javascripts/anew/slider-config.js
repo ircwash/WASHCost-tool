190ae88093a75ce6b6f8fbfea23bdbfd31bd7f57
Johan Tique <johan.tique@codescrum.com>
2013-09-10 10:18:42 -0500

    fix issues in slider-config.js regarding capital slider

--- app/assets/javascripts/anew/slider-config.js
+++ app/assets/javascripts/anew/slider-config.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
         inputCapital = $('input[name="capital"]');
         //default
         inputCapital.val(0);
-        offset_cursor = 10;
+        offset_cursor = 4;
         bar_slider_lenght = capitalSlider.width()*1.00;
         slider_lenght = inputCapital.data('max-value') - inputCapital.data('min-value');
         above_value = inputCapital.data('above-value');
@@ -39,13 +39,13 @@ $(document).ready(function(){
         css_above_value = above_value * step_slider + offset_cursor;
         css_below_value = below_value * step_slider + offset_cursor;
         //labels
-        label_first = $('.labelScale li.first span');
-        label_first.css('margin-left',css_below_value-offset_cursor+"px");
-        label_last = $('.labelScale li.last span');
-        label_last.css('margin-left',css_above_value-(bar_slider_lenght/2)-offset_cursor+"px");
-
+        label_first = $('.labelScale li.first');
+        label_first.css('left',css_below_value-offset_cursor+"px");
+        label_last = $('.labelScale li.last');
+        label_last.css('left',css_above_value-label_last.width()-offset_cursor+"px");
+        //Modifying bag position in X axis
         capitalSlider.css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
-        capitalSlider.css('text-transform','uppercase');
+        $('.slider-container.threshold-marks').css('backgroundPosition', css_above_value+"px 1px, "+css_below_value+"px 0px, 1px 0px");
         slider_display = function(event,ui) {
             inputCapital.val(ui.value);
             $('#capitalValue').html(ui.value);
@@ -56,7 +56,7 @@ $(document).ready(function(){
             change: slider_display,
             slide: slider_display
         });
-        slider_value = capitalSlider.attr("data-slider_value");
+        slider_value = capitalSlider.attr('data-slider_value');
         capitalSlider.slider({value: slider_value});
     }
 
