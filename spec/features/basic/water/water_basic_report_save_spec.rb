fd856f0a63d607c4677454ca8aa4013acdd6a065
Johan Tique <johan.tique@codescrum.com>
2013-09-17 16:12:17 -0500

    add test structure to water basic report

--- spec/features/basic/water/water_basic_report_save_spec.rb
+++ spec/features/basic/water/water_basic_report_save_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
-describe "Report Saving" do
+describe "Water Basic Report Saving" do
   context 'when a signed user saves the water basic report' do
-    visit
+    it 'save a new report' do
+      basic_water = basic_water_questionnaire_completed
+      visit '/cal/water_basic/report'
+      
+    end
+
+    # a more semantic approach to naming the session
+    # could be named "report" too
+    def basic_water_questionnaire_completed
+      basic_water_session({
+                            country: 'BI',
+                            water: 1,
+                            population: 1200,
+                            capital: 59,
+                            recurrent: 8,
+                            time: 1,
+                            quantity: 2,
+                            quality: 2,
+                            reliability: 2,
+                         })
+      basic_water_session
+    end
   end
 end
