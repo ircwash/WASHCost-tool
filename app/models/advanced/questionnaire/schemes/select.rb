4544f920304cd89f13f1c9d1d35e7f953a8abaa4
Johan Tique <johan.tique@codescrum.com>
2013-10-10 09:58:47 -0500

    remove unncessary include in schemes models

--- app/models/advanced/questionnaire/schemes/select.rb
+++ app/models/advanced/questionnaire/schemes/select.rb
@@ -1,7 +1,5 @@
 class Advanced::Questionnaire::Schemes::Select < Advanced::Questionnaire::Schemes::Base
 
-  include Mongoid::Document
-
   field :placeholder,         type: String
   field :options,             type: Array
   field :enable_dont_know,    type: Boolean
--- app/models/advanced/questionnaire/schemes/text_field.rb
+++ app/models/advanced/questionnaire/schemes/text_field.rb
@@ -1,7 +1,5 @@
 class Advanced::Questionnaire::Schemes::TextField < Advanced::Questionnaire::Schemes::Base
 
-  include Mongoid::Document
-
   field :placeholder,         type: String
   field :enable_dont_know,    type: Boolean
 
