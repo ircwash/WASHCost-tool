a8ac5b9bcf6407d8edea14e69015975d90fb465c
Johan Tique <johan.tique@codescrum.com>
2013-10-10 11:56:10 -0500

    remove unncesary files and re organize the layouts files to ember support stuff

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -1,97 +1,10 @@
 # encoding: utf-8
 class AdvancedController < ApplicationController
 
+  layout 'advanced'
 
-  def index
-    if session[:s]
-      copy_session_form_values_to_flash
-    else
-      if params.has_key?(:type) && (params[:type]== 'existing' || params[:type]== 'planned')
-        flash[:type]= params[:type]
-      else
-        redirect_to :controller => "application", :action => "select_advanced"
-      end
-    end
-  end
+  def water
 
-
-  def checkbox_value(key)
-
-    value= params[key]
-
-    checked= false
-    if value.present? && value.to_i> 0
-
-      checked= true
-    end
-
-
-    add_to_session_advanced(key, checked)
-
-    return checked
-  end
-
-  def get_country(country_code)
-
-    country_object= Country.new(country_code)
-    country= "Not Set"
-
-    if(country_object.data == nil)
-      country = nil
-    else
-      country= country_object.name
-      add_to_session_advanced(:country_code, country_code)
-      add_to_session_advanced('country', country)
-    end
-
-    return country
-  end
-
-  def unchecked(key)
-
-    add_to_session_advanced(key, params[key])
-    return params[key]
-
-  end
-
-  def get_total_cost(hardware, software)
-
-    total_cost= 'Please enter both Hardware & Software values'
-    if hardware && software
-      return hardware+software
-    end
-
-  end
-
-
-  def get_indexed_value(i18nPrefix, key)
-
-    text= 'No answer provided'
-
-    if params.has_key?(key) && params[key]!=nil
-
-      index= params[key]
-
-      text= I18n.t i18nPrefix+index
-
-      add_to_session_advanced(key, index)
-
-    end
-
-    return text
-  end
-
-
-  def add_to_session_advanced(key, value)
-    form= session[@session_form].present? ? session[@session_form] : Hash.new(0)
-
-    if !form[key].present?
-      increase_complete_percent(@session_complete)
-    end
-
-    form[key]= value
-
-    session[@session_form]= form
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -60,14 +60,14 @@ WashCostApp::Application.routes.draw do
   match '/cal/sanitation_basic' => 'sanitation_basic#country'
   match '/cal/sanitation_basic/(:action)' => 'sanitation_basic#(:action)', :via => [:get, :post]
 
-  match '/water_advanced' => 'water_advanced#index'
-  match '/water_advanced/(:action)' => 'water_advanced#(:action)', :via=> [:get,:post]
+  match '/cal/water_advanced' => 'advanced#water'
+  match '/cal/water_advanced/(:action)' => 'advanced#(:action)', :via=> [:get,:post]
 
-  match '/sanitation_advanced' => 'sanitation_advanced#index'
-  match '/sanitation_advanced/(:action)' => 'sanitation_advanced#(:action)', :via=> [:get,:post]
+  match '/cal/sanitation_advanced' => 'advanced#sanitation'
+  match '/cal/sanitation_advanced/(:action)' => 'advanced#(:action)', :via=> [:get,:post]
 
-  get "/path" => "controller#get_action"
-  post "/path" => "controller#post_action"
+  get '/path' => 'controller#get_action'
+  post '/path' => 'controller#post_action'
 
   match '/clean_session', :controller => 'application', :action => 'clean_session'
 
