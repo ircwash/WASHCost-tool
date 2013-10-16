ca81f99991fbc15f61a957ba05f586c936479fc6
Johan Tique <johan.tique@codescrum.com>
2013-10-16 15:41:07 -0500

    add water source model to advanced questionnaire

--- app/models/advanced/report/technology.rb
+++ app/models/advanced/report/technology.rb
@@ -2,13 +2,18 @@ class Advanced::Report::Technology
 
   include ActiveAttr::Model
 
-  # Technology type
-  attribute :source_type
   # Number of systems?
   attribute :number_of_systems
   # How many people do the sanitation system(s) serve?
   attribute :expected_number_of_people
   # How many people do the sanitation system(s) actually serve?
   attribute :server_number_of_people
+  # Technology type
+  attribute :water_source
+
+  def water_source_attributes=(attributes)
+    self.water_source = Advanced::Report::WaterSource.new(attributes)
+  end
+
 
 end
---
+++ app/models/advanced/report/water_source.rb
@@ -0,0 +1,19 @@
+class Advanced::Report::WaterSource
+
+  include ActiveAttr::Model
+
+  # water source?
+  attribute :source
+  # if surface water - define primary source | if ground water - define primary source
+  attribute :primary_source
+  # if drilled well with nnon-mechanised pump. Define most common pump
+  attribute :common_pump
+  # how is water typically stored
+  attribute :storing
+  # How is the water tipically treated
+  attribute :treating
+  # Type os power supply
+  attribute :supply
+  # Length of distribution line (Km)
+  attribute :length
+end
