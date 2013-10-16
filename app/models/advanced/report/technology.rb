96af737f13dba688cac75c080d8ee2e519dbba03
Johaned <johan.tique@codescrum.com>
2013-10-16 12:06:37 -0500

    create the technology active model

--- app/models/advanced/report/technology.rb
+++ app/models/advanced/report/technology.rb
@@ -1,3 +1,14 @@
-class Technology
-  # To change this template use File | Settings | File Templates.
+class Advanced::Report::Technology
+
+  include ActiveAttr::Model
+
+  # Technology type
+  attribute :source_type
+  # Number of systems?
+  attribute :number_of_systems
+  # How many people do the sanitation system(s) serve?
+  attribute :expected_number_of_people
+  # How many people do the sanitation system(s) actually serve?
+  attribute :server_number_of_people
+
 end
