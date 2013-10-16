bd6f5b81355ff91177a786001743f12f7f4fde15
Johaned <johan.tique@codescrum.com>
2013-10-16 12:07:15 -0500

    create the descriptor active model in advanced report

--- app/models/advanced/report/descriptor.rb
+++ app/models/advanced/report/descriptor.rb
@@ -1,3 +1,44 @@
-class Descriptor
-  # To change this template use File | Settings | File Templates.
+class Advanced::Report::Descriptor
+
+  include ActiveAttr::Model
+
+  # Report Name
+  attribute :title
+  # Does the sanitation service already exist or is it planned?
+  attribute :sanitation_service_type
+  # Country
+  attribute :country
+  # Region
+  attribute :region
+  # City
+  attribute :city
+  # Description of area type?
+  attribute :area_type
+  # What is the population density of the service area?
+  attribute :population_density
+  # Which of these options best describes how the sanitation service is managed?
+  attribute :service_management
+  # Who finances the construction of the sanitation system?
+  attribute :service_financing
+  # Who undertakes the operation and maintenance of the infrastructure?
+  attribute :service_maintenance
+  # Who has/will have responsibility for checking and enforcing the standard of sanitation services?
+  attribute :service_standarization
+  # Who covers the cost of rehabilitation when larger repairs are needed (pit/tank emptying)?
+  attribute :service_costing
+  # What is the average annual household income in the service area
+  attribute :average_household_income
+  # Average household size
+  attribute :average_household_size
+  # Has many technologies
+  attribute :technologies
+
+  def technologies_attributes=(_technologies)
+    technologies = []
+    technologies.keys.each do |index|
+      technologies << Advanced::Report::Technology.new(_technologies[index])
+    end
+  end
+
+
 end
