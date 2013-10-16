4b5182a54b0351a41728ade38bcee86176522815
Johaned <johan.tique@codescrum.com>
2013-10-16 12:48:58 -0500

    create fictitious form to real advanced form (controller and view)

--- app/controllers/advanced/questionnaires_controller.rb
+++ app/controllers/advanced/questionnaires_controller.rb
@@ -1,3 +1,11 @@
-class QuestionnairesController
-  # To change this template use File | Settings | File Templates.
+class Advanced::QuestionnairesController < ApplicationController
+  def index
+    @descriptor = Advanced::Report::Descriptor.new
+    technologies = []
+    3.times.each do ||
+      technology = Advanced::Report::Technology.new
+      technologies << technology
+    end
+    @descriptor.technologies = technologies
+  end
 end
