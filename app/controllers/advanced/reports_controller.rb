6a41c7865b61870fc573a86a04b5942cdd28b569
Johan Tique <johan.tique@codescrum.com>
2013-10-17 11:20:23 -0500

    add all required options to advanced fictitious form

--- app/controllers/advanced/reports_controller.rb
+++ app/controllers/advanced/reports_controller.rb
@@ -2,6 +2,7 @@ class Advanced::ReportsController < ApplicationController
 
   def save
     @descriptor = Advanced::Report::Descriptor.new(params[:advanced_report_descriptor])
+    binding.pry
     redirect_to root_path
   end
 
