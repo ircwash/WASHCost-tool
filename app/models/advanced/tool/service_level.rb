1e402fcadb35e15ec20104e7f55ce6fa81cf2234
Johaned <johan.tique@codescrum.com>
2013-10-18 03:48:03 -0500

    add numeric values to service level

--- app/models/advanced/tool/service_level.rb
+++ app/models/advanced/tool/service_level.rb
@@ -3,24 +3,24 @@ class Advanced::Tool::ServiceLevel
   include ActiveAttr::Model
 
   # What is the typical average distance in minutes from households to their main formal water source?
-  attribute :accesibility_distance
+  attribute :accesibility_distance, :type => Integer
   # Are there long queues at the water source OR are many more people using it than originally planned for?
-  attribute :accesibility_queues
+  attribute :accesibility_queues, :type => Integer
   # Does the service accessibility meet the national norms?
-  attribute :accesibility_norms
+  attribute :accesibility_norms, :type => Integer
   # What is the actual per capita use of water from formal sources per day (domestic use only)
-  attribute :quantity_use
+  attribute :quantity_use, :type => Integer
   # Does the service quantity meet the national norms?
-  attribute :quantity_norms
+  attribute :quantity_norms, :type => Integer
   # How often is water quality testing done?
-  attribute :quality_testing
+  attribute :quality_testing, :type => Integer
   # Does the service quality meet the national norms?
-  attribute :quality_norms
+  attribute :quality_norms, :type => Integer
   # How reliable is the service?
-  attribute :reliability
+  attribute :reliability, :type => Integer
   # Does the service reliability meet the national norms?
-  attribute :reliability_norms
+  attribute :reliability_norms, :type => Integer
   # What percentage of people in the service area receive this service?
-  attribute :percentage_using
+  attribute :percentage_using, :type => Float
 
 end
