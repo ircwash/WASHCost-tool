d53763fb1ec7d9de7bab139909b20a97314c12aa
Johan Tique <johan.tique@codescrum.com>
2013-11-05 15:58:22 -0500

    update spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| load f}
 
   RSpec.configure do |config|
 
