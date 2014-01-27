18c425b993c5fc20a40c485f11f4b4dab0b467c3
Tom Chislett <tchislett@brilliantbasics.com>
2014-01-27 17:00:07 +0000

    removed uneccessary selection model

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -7,21 +7,28 @@ class CalculatorsController < ApplicationController
   end
 
   def selection
-    @selection = Selection::ToolSelection.new(params[:selection])
-    I18n.locale =  @selection.lang
-    case @selection.index
-      when 0
-        session[:water_basic_form] = {}
-        session[:water_basic_complete] = {}
-        redirect_to basic_water_path( I18n.locale )
-      when 2
-        session[:sanitation_basic_form] = {}
-        session[:sanitation_basic_complete] = {}
-        redirect_to basic_sanitation_path( I18n.locale )
-      else
-        flash[:alert] = 'We will publish the advanced Calculator very soon. Please your email and we will let you know when the tool is ready.'
-        #redirect_to cal_path, flash: { popup_class_name: '.notification.subscriber' }
-        redirect_to new_user_registration_path
+
+    case params[ :tool_level ]
+      when 'basic'
+        case params[ :tool_name ]
+          when 'water'
+            session[:water_basic_form] = {}
+            session[:water_basic_complete] = {}
+
+            redirect_to basic_water_path( I18n.locale )
+          when 'sanitation'
+            session[:sanitation_basic_form] = {}
+            session[:sanitation_basic_complete] = {}
+
+            redirect_to basic_sanitation_path( I18n.locale )
+        end
+      when 'advanced'
+        case params[ :tool_name ]
+          when 'water'
+
+          when 'sanitation'
+
+        end
     end
   end
 
