fe527c33705e958e8cc39b359c1d082a033f22ad
Luca De Angeli <ldeangeli@brilliantbasics.com>
2014-01-27 18:53:42 +0000

    removed listener for video image placeholder

--- app/assets/javascripts/washcost/homepage.js
+++ app/assets/javascripts/washcost/homepage.js
@@ -5,16 +5,16 @@ $( document ).ready( function()
   var _info_tab = $('.info_tab');
 
 
-  function init()
-  {
-    // bind events
-    $( '.homage--video--placeholder' ).on( 'click', function()
-    {
-      $( this ).replaceWith( '<iframe src="https://player.vimeo.com/video/73270180?autoplay=true" width="800" height="451" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>' );
-    } );
-  }
+  // function init()
+  // {
+  //   bind events
+  //   $( '.homage--video--placeholder' ).on( 'click', function()
+  //   {
+  //     $( this ).replaceWith( '<iframe src="https://player.vimeo.com/video/73270180?autoplay=true" width="800" height="451" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>' );
+  //   } );
+  // }
 
 
-  init();
+  // init();
 } );
 
