1cedb80cf04e338181ab7d1b943da2003ba4a0d6
Luca De Angeli <ldeangeli@brilliantbasics.com>
2014-01-28 14:56:48 +0000

    comments cleanup

--- app/assets/javascripts/washcost/homepage.js
+++ app/assets/javascripts/washcost/homepage.js
@@ -4,17 +4,5 @@ $( document ).ready( function()
 
   var _info_tab = $('.info_tab');
 
-
-  // function init()
-  // {
-  //   bind events
-  //   $( '.homage--video--placeholder' ).on( 'click', function()
-  //   {
-  //     $( this ).replaceWith( '<iframe src="https://player.vimeo.com/video/73270180?autoplay=true" width="800" height="451" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>' );
-  //   } );
-  // }
-
-
-  // init();
 } );
 
