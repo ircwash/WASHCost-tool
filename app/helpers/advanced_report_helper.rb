2ae5850165a4f38cdee36423fab33f2940c6b71d
Tom Chislett <tchislett@brilliantbasics.com>
2014-01-31 14:51:36 +0000

    fixed issues with incomplete reports

---
+++ app/helpers/advanced_report_helper.rb
@@ -0,0 +1,28 @@
+module AdvancedReportHelper
+
+  def report_totals_available
+    report_expenditure_totals_available != false || report_service_level_summary_available != false
+  end
+
+  def report_expenditure_totals_available
+    @questionnaire.total_expenditure_for_years(30) != nil && @questionnaire.total_inputted_expenditure_per_person_per_year != nil && @questionnaire.total_expenditure_delta_per_person_per_year != nil
+  end
+
+  def report_service_level_summary_available
+    @questionnaire.people_with_service_meeting_national_standard != nil
+  end
+
+
+  def report_integer_value_for( value )
+    value != nil ? "#{value.to_i}" : t( 'report.no_data' )
+  end
+
+  def report_currency_value_for( value )
+    value != nil ? "US$ #{value.to_f.round(2)}" : t( 'report.no_data' )
+  end
+
+  def report_percentage_value_for( value )
+    value != nil ? "#{value.to_f.round(2)}%" : t( 'report.no_data' )
+  end
+
+end
--- app/models/advanced_water_questionnaire.rb
+++ app/models/advanced_water_questionnaire.rb
@@ -135,149 +135,325 @@ class AdvancedWaterQuestionnaire < Session
   # CALCULATIONS
 
   def total_population
-    supply_system_technologies.each_with_index.map{ |s,i| system_population_actual[i].to_f }.inject(:+)
+    if supply_system_technologies.count > 0 && system_population_actual.count == supply_system_technologies.count
+      supply_system_technologies.each_with_index.map{ |s,i| system_population_actual[i].to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   def total_expenditure_for_years( years )
-    years * ( supply_system_technologies.each_with_index.map{ |s,i| actual_hardware_expenditure[i].to_f + actual_software_expenditure[i].to_f }.inject(:+) + ( expected_operation_expenditure_per_person_per_year + expected_capital_maintenance_expenditure_per_person_per_year + expected_direct_support_cost_per_person_per_year ) * total_population + direct_support_cost.to_f + indirect_support_cost.to_f )
+    if hardware_and_software_expenditure != nil && expected_operation_expenditure_per_person_per_year != nil && expected_capital_maintenance_expenditure_per_person_per_year != nil && expected_direct_support_cost_per_person_per_year != nil && total_population != nil && direct_support_cost != nil && indirect_support_cost != nil
+      years * ( hardware_and_software_expenditure + ( expected_operation_expenditure_per_person_per_year + expected_capital_maintenance_expenditure_per_person_per_year + expected_direct_support_cost_per_person_per_year ) * total_population + direct_support_cost.to_f + indirect_support_cost.to_f )
+    else
+      nil
+    end
+  end
+
+  def hardware_and_software_expenditure
+    if supply_system_technologies.count > 0 && actual_hardware_expenditure.count == supply_system_technologies.count && actual_software_expenditure.count == supply_system_technologies.count
+      supply_system_technologies.each_with_index.map{ |s,i| actual_hardware_expenditure[i].to_f + actual_software_expenditure[i].to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   # expenditure
 
   def operation_expenditure_per_person_per_year
-    supply_system_technologies.each_with_index.map{ |s,i| minor_operation_expenditure[i].to_f / system_population_actual[i].to_f }.inject(:+)
+    if supply_system_technologies.count > 0 && minor_operation_expenditure.count == supply_system_technologies.count && system_population_actual.count == supply_system_technologies.count
+      supply_system_technologies.each_with_index.map{ |s,i| minor_operation_expenditure[i].to_f / system_population_actual[i].to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   def capital_maintenance_expenditure_per_person_per_year
-    supply_system_technologies.each_with_index.map{ |s,i| capital_maintenance_expenditure[i].to_f / system_population_actual[i].to_f }.inject(:+)
+    if supply_system_technologies.count > 0 && capital_maintenance_expenditure.count == supply_system_technologies.count && system_population_actual.count == supply_system_technologies.count
+      supply_system_technologies.each_with_index.map{ |s,i| capital_maintenance_expenditure[i].to_f / system_population_actual[i].to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   def cost_of_capital_per_person_per_year
-    supply_system_technologies.each_with_index.map{ |s,i| ( loan_cost[i].to_f * [ loan_payback_period[i].to_i, 30 ].min / 30) / system_population_actual[i].to_f }.inject(:+)
+    if supply_system_technologies.count > 0 && loan_cost.count == supply_system_technologies.count && loan_payback_period.count == supply_system_technologies.count && system_population_actual.count == supply_system_technologies.count
+      supply_system_technologies.each_with_index.map{ |s,i| ( loan_cost[i].to_f * [ loan_payback_period[i].to_i, 30 ].min / 30) / system_population_actual[i].to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   def direct_support_cost_per_person_per_year
-    direct_support_cost.to_f / total_population
+    if direct_support_cost != nil && total_population != nil
+      direct_support_cost.to_f / total_population
+    else
+      nil
+    end
   end
 
   def indirect_support_cost_per_person_per_year
-    indirect_support_cost.to_f / total_population
+    if indirect_support_cost != nil && total_population != nil
+      indirect_support_cost.to_f / total_population
+    else
+      nil
+    end
   end
 
   def total_inputted_expenditure_per_person_per_year
-    operation_expenditure_per_person_per_year + capital_maintenance_expenditure_per_person_per_year + cost_of_capital_per_person_per_year + direct_support_cost_per_person_per_year + indirect_support_cost_per_person_per_year
+    if operation_expenditure_per_person_per_year != nil && capital_maintenance_expenditure_per_person_per_year != nil && cost_of_capital_per_person_per_year != nil && direct_support_cost_per_person_per_year != nil && indirect_support_cost_per_person_per_year != nil
+      operation_expenditure_per_person_per_year + capital_maintenance_expenditure_per_person_per_year + cost_of_capital_per_person_per_year + direct_support_cost_per_person_per_year + indirect_support_cost_per_person_per_year
+    else
+      nil
+    end
   end
 
   def expected_operation_expenditure_per_person_per_year
-    benchmark_minor_operation_expenditure.to_f / total_population
+    if benchmark_minor_operation_expenditure != nil && total_population != nil
+      benchmark_minor_operation_expenditure.to_f / total_population
+    else
+      nil
+    end
   end
 
   def expected_capital_maintenance_expenditure_per_person_per_year
-    benchmark_capital_maintenance_expenditure.to_f / total_population
+    if benchmark_capital_maintenance_expenditure != nil && total_population != nil
+      benchmark_capital_maintenance_expenditure.to_f / total_population
+    else
+      nil
+    end
   end
 
   def expected_direct_support_cost_per_person_per_year
-    benchmark_direct_support_cost.to_f / total_population
+    if benchmark_direct_support_cost != nil && total_population != nil
+      benchmark_direct_support_cost.to_f / total_population
+    else
+      nil
+    end
   end
 
   def total_expected_expenditure_per_person_per_year
-    expected_operation_expenditure_per_person_per_year + expected_capital_maintenance_expenditure_per_person_per_year + cost_of_capital_per_person_per_year + expected_direct_support_cost_per_person_per_year + indirect_support_cost_per_person_per_year
+    if expected_operation_expenditure_per_person_per_year != nil && expected_capital_maintenance_expenditure_per_person_per_year != nil && cost_of_capital_per_person_per_year != nil && expected_direct_support_cost_per_person_per_year != nil && indirect_support_cost_per_person_per_year != nil
+      expected_operation_expenditure_per_person_per_year + expected_capital_maintenance_expenditure_per_person_per_year + cost_of_capital_per_person_per_year + expected_direct_support_cost_per_person_per_year + indirect_support_cost_per_person_per_year
+    else
+      nil
+    end
+  end
+
+  def expected_operation_expenditure_delta_per_person_per_year
+    if operation_expenditure_per_person_per_year != nil && expected_operation_expenditure_per_person_per_year != nil
+      operation_expenditure_per_person_per_year - expected_operation_expenditure_per_person_per_year
+    else
+      nil
+    end
+  end
+
+  def expected_capital_maintenance_expenditure_delta_per_person_per_year
+    if capital_maintenance_expenditure_per_person_per_year != nil && expected_capital_maintenance_expenditure_per_person_per_year != nil
+      capital_maintenance_expenditure_per_person_per_year - expected_capital_maintenance_expenditure_per_person_per_year
+    else
+      nil
+    end
+  end
+
+  def expenditure_of_direct_support_delta_per_person_per_year
+    if direct_support_cost_per_person_per_year != nil && direct_support_cost_per_person_per_year != nil
+      direct_support_cost_per_person_per_year - direct_support_cost_per_person_per_year
+    else
+      nil
+    end
+  end
+
+  def total_expenditure_delta_per_person_per_year
+    if total_inputted_expenditure_per_person_per_year != nil && total_expected_expenditure_per_person_per_year != nil
+      total_inputted_expenditure_per_person_per_year - total_expected_expenditure_per_person_per_year
+    else
+      nil
+    end
   end
 
   # affordability
 
   def annual_household_income_per_person
-    annual_household_income.to_f / household_size.to_f
+    if annual_household_income != nil && household_size != nil
+      annual_household_income.to_f / household_size.to_f
+    else
+      nil
+    end
   end
 
   def people_with_service_meeting_national_standard
-    service_level_name.each_with_index.map{ |sl,i| ( national_accessibility_norms[i].to_i == 0 || national_quantity_norms[i].to_i == 0 || national_quality_norms[i].to_i == 0 || national_reliability_norms[i].to_i == 0 ) ? service_level_share[i].to_i : 0 }.inject(:+)
+    if service_level_name.count > 0 && ( national_accessibility_norms.count > 0 || national_quantity_norms.count > 0 || national_quality_norms.count > 0 || national_reliability_norms.count > 0 )
+      service_level_name.each_with_index.map{ |sl,i| ( national_accessibility_norms[i].to_i == 0 || national_quantity_norms[i].to_i == 0 || national_quality_norms[i].to_i == 0 || national_reliability_norms[i].to_i == 0 ) ? service_level_share[i].to_i : 0 }.inject(:+)
+    else
+      nil
+    end
   end
 
   def total_actual_users
-    system_population_actual.map{ |spd| spd.to_f }.inject(:+)
+    if system_population_actual.count > 0
+      system_population_actual.map{ |spd| spd.to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   def total_designed_users
-    system_population_design.map{ |spa| spa.to_f }.inject(:+)
+    if system_population_design.count > 0
+      system_population_design.map{ |spa| spa.to_f }.inject(:+)
+    else
+      nil
+    end
   end
 
   # affordability inputted actual users
 
   def annual_operational_expenditure_for_actual_users
-    minor_operation_expenditure.each.map{ |moe| moe.to_f }.inject(:+) / total_actual_users
+    if minor_operation_expenditure.count > 0 && total_actual_users != nil
+      minor_operation_expenditure.each.map{ |moe| moe.to_f }.inject(:+) / total_actual_users
+    else
+      nil
+    end
   end
 
   def annual_operational_expenditure_for_actual_users_as_percentage_of_household_income
-    100 * annual_operational_expenditure_for_actual_users / annual_household_income.to_f
+    if annual_operational_expenditure_for_actual_users != nil && annual_household_income != nil
+      100 * annual_operational_expenditure_for_actual_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   def annual_capital_maintenance_expenditure_for_actual_users
-    capital_maintenance_expenditure.each.map{ |cme| cme.to_f }.inject(:+) / total_actual_users
+    if capital_maintenance_expenditure.count > 0 && total_actual_users != nil
+      capital_maintenance_expenditure.each.map{ |cme| cme.to_f }.inject(:+) / total_actual_users
+    else
+      nil
+    end
   end
 
   def annual_capital_maintenance_expenditure_for_actual_users_as_percentage_of_household_income
-    100 * annual_capital_maintenance_expenditure_for_actual_users / annual_household_income.to_f
+    if annual_capital_maintenance_expenditure_for_actual_users != nil && annual_household_income != nil
+      100 * annual_capital_maintenance_expenditure_for_actual_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   def total_annual_expenditure_for_actual_users
-    annual_operational_expenditure_for_actual_users + annual_capital_maintenance_expenditure_for_actual_users
+    if annual_operational_expenditure_for_actual_users != nil && annual_capital_maintenance_expenditure_for_actual_users != nil
+      annual_operational_expenditure_for_actual_users + annual_capital_maintenance_expenditure_for_actual_users
+    else
+      nil
+    end
   end
 
   def total_annual_expenditure_for_actual_users_as_percentage_of_household_income
-    100 * total_annual_expenditure_for_actual_users / annual_household_income.to_f
+    if total_annual_expenditure_for_actual_users != nil && annual_household_income != nil
+      100 * total_annual_expenditure_for_actual_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   # affordability inputted designed users
 
   def annual_operational_expenditure_for_designed_users
-    minor_operation_expenditure.each.map{ |moe| moe.to_f }.inject(:+) / total_designed_users
+    if minor_operation_expenditure.count > 0 && total_designed_users != nil
+      minor_operation_expenditure.each.map{ |moe| moe.to_f }.inject(:+) / total_designed_users
+    else
+      nil
+    end
   end
 
   def annual_operational_expenditure_for_designed_users_as_percentage_of_household_income
-    100 * annual_operational_expenditure_for_designed_users / annual_household_income.to_f
+    if annual_operational_expenditure_for_designed_users != nil && annual_household_income != nil
+      100 * annual_operational_expenditure_for_designed_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   def annual_capital_maintenance_expenditure_for_designed_users
-    capital_maintenance_expenditure.each.map{ |cme| cme.to_f }.inject(:+) / total_designed_users
+    if capital_maintenance_expenditure.count > 0 && total_designed_users != nil
+      capital_maintenance_expenditure.each.map{ |cme| cme.to_f }.inject(:+) / total_designed_users
+    else
+      nil
+    end
   end
 
   def annual_capital_maintenance_expenditure_for_designed_users_as_percentage_of_household_income
-    100 * annual_capital_maintenance_expenditure_for_designed_users / annual_household_income.to_f
+    if annual_capital_maintenance_expenditure_for_designed_users != nil && annual_household_income != nil
+      100 * annual_capital_maintenance_expenditure_for_designed_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   def total_annual_expenditure_for_designed_users
-    annual_operational_expenditure_for_designed_users + annual_capital_maintenance_expenditure_for_designed_users
+    if annual_operational_expenditure_for_designed_users != nil && annual_capital_maintenance_expenditure_for_designed_users != nil
+      annual_operational_expenditure_for_designed_users + annual_capital_maintenance_expenditure_for_designed_users
+    else
+      nil
+    end
   end
 
   def total_annual_expenditure_for_designed_users_as_percentage_of_household_income
-    100 * total_annual_expenditure_for_designed_users / annual_household_income.to_f
+    if total_annual_expenditure_for_designed_users != nil && annual_household_income != nil
+      100 * total_annual_expenditure_for_designed_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   # affordability expected actual users
 
   def expected_annual_operational_expenditure_for_actual_users
-    benchmark_minor_operation_expenditure / total_actual_users
+    if benchmark_minor_operation_expenditure != nil && total_actual_users != nil
+      benchmark_minor_operation_expenditure / total_actual_users
+    else
+      nil
+    end
   end
 
   def expected_annual_operational_expenditure_for_actual_users_as_percentage_of_household_income
-    100 * expected_annual_operational_expenditure_for_actual_users / annual_household_income.to_f
+    if expected_annual_operational_expenditure_for_actual_users != nil && annual_household_income != nil
+      100 * expected_annual_operational_expenditure_for_actual_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   def expected_annual_capital_maintenance_expenditure_for_actual_users
-    benchmark_capital_maintenance_expenditure / total_actual_users
+    if benchmark_capital_maintenance_expenditure != nil && total_actual_users != nil
+      benchmark_capital_maintenance_expenditure / total_actual_users
+    else
+      nil
+    end
   end
 
   def expected_annual_capital_maintenance_expenditure_for_actual_users_as_percentage_of_household_income
-    100 * expected_annual_capital_maintenance_expenditure_for_actual_users / annual_household_income.to_f
+    if expected_annual_capital_maintenance_expenditure_for_actual_users != nil && annual_household_income != nil
+      100 * expected_annual_capital_maintenance_expenditure_for_actual_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
   def total_expected_annual_expenditure_for_actual_users
-    expected_annual_operational_expenditure_for_actual_users + expected_annual_capital_maintenance_expenditure_for_actual_users
+    if expected_annual_operational_expenditure_for_actual_users != nil && expected_annual_capital_maintenance_expenditure_for_actual_users != nil
+      expected_annual_operational_expenditure_for_actual_users + expected_annual_capital_maintenance_expenditure_for_actual_users
+    else
+      nil
+    end
   end
 
   def total_expected_annual_expenditure_for_actual_users_as_percentage_of_household_income
-    100 * total_expected_annual_expenditure_for_actual_users / annual_household_income.to_f
+    if total_expected_annual_expenditure_for_actual_users != nil && annual_household_income != nil
+      100 * total_expected_annual_expenditure_for_actual_users / annual_household_income.to_f
+    else
+      nil
+    end
   end
 
 
