854c51db5c52b2f483d5c421a98111b2b95e1e7e
Tom Chislett <tchislett@brilliantbasics.com>
2014-02-05 10:03:21 +0000

    fix for broken basic calculator footer

--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -5,7 +5,7 @@ module CalculatorsHelper
       when 'basic/water'
         basic_water_report_path( I18n.locale )
       when 'basic/sanitation'
-        basic_sanitation_report_path( I18n.locale )
+        basic_water_report_path( I18n.locale )
       when 'advanced/water'
         advanced_water_report_path( I18n.locale )
       when 'advanced/sanitation'
@@ -16,9 +16,9 @@ module CalculatorsHelper
   def footer_save_report_url
     case controller_path
       when 'basic/water'
-        basic_water_save_report_path( I18n.locale )
+        basic_report_questionnaire_path( I18n.locale, :tool_name => 'water' )
       when 'basic/sanitation'
-        basic_sanitation_save_report_path( I18n.locale )
+        basic_report_questionnaire_path( I18n.locale, :tool_name => 'sanitation' )
       when 'advanced/water'
         advanced_water_save_report_path( I18n.locale )
       when 'advanced/sanitation'
