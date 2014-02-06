84754103001364b7e4fee10f541ce5e1bc4429b6
Tom <tchislett@brilliantbasics.com>
2014-02-06 22:06:22 +0000

    removed uneccessary models

--- app/models/subscriber.rb
+++
@@ -1,8 +0,0 @@
-class Subscriber
-  include Mongoid::Document
-
-  validates_presence_of :email
-  validates_format_of :email, with: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-
-  field :email
-end
