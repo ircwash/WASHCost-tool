2cef225bd65bbaae84b57496f291a0d084c0303d
Tom Chislett <tchislett@brilliantbasics.com>
2014-02-11 15:46:30 +0000

    added info tabs to all water basic pages

--- app/assets/javascripts/washcost/infotab.js
+++ app/assets/javascripts/washcost/infotab.js
@@ -13,8 +13,10 @@ $( document ).ready( function()
     {
       _info_tab.on( 'click', '.info_tab--icon', function( event )
       {
+        var width = $( window ).width() >= 600 ? -30 + 2 * _info_tab.parent().width() / 3 : _info_tab.parent().width();
+
         // animate tab
-        _info_tab.toggleClass( 'info_tab-open' );
+        _info_tab.toggleClass( 'info_tab-open' ).find( '.info_tab--container' ).css( { width:width } );
       } );
     }
   }
