0fc863369bb0fe539e597de6ae8b15ce314ee946
Ben Howdle <benhowdle89@gmail.com>
2014-03-01 12:53:43 +0000

    after sign in URL

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,5 +1,15 @@
 class Authentication::SessionsController < Devise::SessionsController
 
+  after_filter :store_location
+
   layout "general"
+
+  def store_location
+    session[:previous_url] = request.referrer || root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
 
 end
