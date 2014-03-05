aee6334d8419b686d9280783307f5cb5632e1483
Ben Howdle <benhowdle89@gmail.com>
2014-03-05 16:19:49 +0000

    report messages

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -3,14 +3,30 @@ class CalculatorController < ApplicationController
   protected
 
   def store_report( title, level, type, questionnaire )
-    current_user.user_reports << UserReport.new(
+    new_report = UserReport.new(
       :title => title,
       :type  => type,
       :level => level,
       :questionnaire => questionnaire
     )
-
-    redirect_to dashboard_index_path
+    arr = current_user.user_reports.select {|report| report["title"] == title }
+    if(!new_report.valid? || arr.any?)
+      @report = UserReport.new(params[:user_report])
+      new_report.errors.messages.each do |key, value|
+        @report.errors.add(key, value.first)
+      end
+      if(arr.any?)
+        @report.errors.add(:unique_report, t('mongoid.errors.models.user_report.attributes.title.unique_report'))
+      end
+      render layout: 'general', template: 'shared/save_report'
+    else
+      current_user.user_reports << UserReport.new(
+        :title => title,
+        :type  => type,
+        :level => level,
+        :questionnaire => questionnaire
+      )
+      redirect_to dashboard_index_path
+    end
   end
-
 end
--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -2,8 +2,12 @@ class UserReport < Report
 
   embedded_in :user
 
-  validates_presence_of :title
+  # validates_presence_of :title
 
   field :title, :type => String
+
+  validates :title,
+    :uniqueness => {:message => :unique_report},
+    :presence => {:message => :report_title_missing}
 
 end
