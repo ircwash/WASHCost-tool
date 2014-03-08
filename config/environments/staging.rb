b67c16027d071419e623c9b34ae7fc0a99531650
Ben Howdle <benhowdle89@gmail.com>
2014-03-08 18:42:45 +0000

    staging emails

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,4 +64,19 @@ WashCostApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "test.washcost.org" }
+
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => "noreplywashcostcalculator@gmail.com",
+    :password       => "password999"
+  }
+
 end
