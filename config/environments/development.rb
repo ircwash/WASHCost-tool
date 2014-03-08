aa7fc2e06c333ffa7eec7f5e2dd71331ea505b67
Ben Howdle <benhowdle89@gmail.com>
2014-03-08 15:14:44 +0000

    mail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,20 @@ WashCostApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Mail settings
+  config.action_mailer.default_url_options = { :host => 'dev.washcost.org' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "89.206.255.82",
+    :port    => 25,
+    :user_name  => "passwordreset",
+    :password   => "Native2014",
+    :authentication     => "plain",
+    :enable_starttls_auto       => true,
+    :openssl_verify_mode => 'none'
+  }
+
 end
