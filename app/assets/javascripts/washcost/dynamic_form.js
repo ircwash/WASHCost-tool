73e9b35bf52fe4fa7256d8fc76dfe7601f0ade11
Neil Kinnish <neilkinnish@Neils-MacBook-Pro-2.local>
2014-04-14 14:01:32 +0100

    updated advanced service level so as data correctly saved, no response defaults to dont know on submit

--- app/assets/javascripts/washcost/dynamic_form.js
+++ app/assets/javascripts/washcost/dynamic_form.js
@@ -10,8 +10,15 @@ $( document ).ready( function()
       var form = $( this ).parents( '[data-dynamic_form]' ),
           url  = form.data( 'dynamic_form' );
 
-      form.ajaxSubmit( { url:url, success:function( result )
-      {
+      // Forces a selection (default = Don't know) for any enabled selects that have not been set on submit
+      if ($('form#new_advanced_water_questionnaire').length > 0) {
+        $('form#new_advanced_water_questionnaire').find( 'select:enabled' ).each( function () { 
+          if (this.selectedIndex === 0)
+            this.selectedIndex = this.length-1;
+        });
+      }
+
+      form.ajaxSubmit( { url:url, success:function( result ) {
         // trigger notification
         form.trigger( 'ajax_submit', result );
       } } );
