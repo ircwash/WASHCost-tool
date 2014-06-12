69dd64cb4aa3d321152eb0977934cbb009b38aef
Neil Kinnish <hello@neilkinnish.com>
2014-06-12 16:21:24 +0100

    updating graph logic

--- app/assets/javascripts/washcost/chart.js
+++ app/assets/javascripts/washcost/chart.js
@@ -86,10 +86,12 @@ $( document ).ready( function()
             var p = ((i % system_lifespan[j]) === 0) ? actual_hardware[j] : 0;
             n += p;
           }
-          capital_maintenance_expenditure[i-1] = n;
+          capital_maintenance_expenditure[i-1] = { year:i , cost: n }; 
         }
 
         chartObjects.push({ seriesName: 'capital_maintenance_expenditure', color: '#333333', data: capital_maintenance_expenditure });
+
+        //console.log( chartObjects );
 
         chart.render( chartObjects );
       }
